ce13cbd930a0606a5ba42d06ef588f53122e523e
Chris J. Karr <chris@audacious-software.com>
2016-08-06 18:37:41 +0000

    Fixing map display over HTTPS

--- map_app/admin.py
+++ map_app/admin.py
@@ -1,32 +1,42 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from .models import SitePlan, PlanDetail, PlanOverlay, AreaOfInterest, MediaAttachment
 
 @admin.register(SitePlan)
-class SitePlanAdmin(admin.ModelAdmin):
+class SitePlanAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'location',)
     search_fields = ['name']
 
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
+
 @admin.register(PlanDetail)
-class PlanDetailAdmin(admin.ModelAdmin):
+class PlanDetailAdmin(admin.OSMGeoAdmin):
     list_display = ('plan', 'name', 'priority',)
     list_filter = ('plan', 'name', 'priority', 'type',)
     search_fields = ['name', 'value', 'type']
 
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
+
 @admin.register(PlanOverlay)
-class PlanOverlayAdmin(admin.ModelAdmin):
+class PlanOverlayAdmin(admin.OSMGeoAdmin):
     list_display = ('plan', 'name', 'z_index', 'center_point', 'scale', 'rotation',)
     list_filter = ('plan', 'z_index',)
     search_fields = ['name']
 
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
+
 @admin.register(AreaOfInterest)
-class AreaOfInterestAdmin(admin.ModelAdmin):
+class AreaOfInterestAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'overlay', 'location', 'is_point', 'tags', 'created', 'updated')
     list_filter = ('created', 'updated', 'tags',)
     search_fields = ['name']
 
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
+
 @admin.register(MediaAttachment)
-class MediaAttachmentAdmin(admin.ModelAdmin):
+class MediaAttachmentAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'mime_type', 'created', 'location',)
     list_filter = ('created', 'mime_type',)
     search_fields = ['name', 'mime_type']
+
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
--- workers_app/admin.py
+++ workers_app/admin.py
@@ -1,15 +1,19 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from .models import Worker, WorkerDetail
 
 @admin.register(Worker)
-class WorkerAdmin(admin.ModelAdmin):
+class WorkerAdmin(admin.OSMGeoAdmin):
     list_display = ('name', 'last_location', 'tags',)
     list_filter = ('tags',)
     search_fields = ['name', 'tags']
+    
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
 
 @admin.register(WorkerDetail)
-class WorkerDetailAdmin(admin.ModelAdmin):
+class WorkerDetailAdmin(admin.OSMGeoAdmin):
     list_display = ('worker', 'name', 'value', 'priority', 'type')
     list_filter = ('name', 'type', 'priority',)
     search_fields = ['name', 'value']
+
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/lib/OpenLayers.js'
